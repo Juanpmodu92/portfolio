5b0cc3a8bd628e712cae7f384f7e0240ceb54ee9
Juanpmodu92 <juanpmodu92@gmail.com>
2025-01-30 16:23:19 -0500

    Primera subida de mi portafolio

---
+++ script.js
@@ -0,0 +1,41 @@
+//FUNCIÓN QUE APLICA EL ESTILO A LA OPCIÓN SELECCIONADA EN EL MENÚ Y QUITA LA PREVIAMENTE SELECCIONADA
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links a')
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    //DESAPARECEMOS EL MENÚ UNA VEZ SELECCIONADA LA OPCIÓN EN EL MODO RESPONSIVE 
+    var x =document.getElementById("nav")
+    x.className = ""; 
+
+}
+//FUNCIÓN PARA EL MENÚ RESPONSIVE
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+    } else{
+        x.className = "";
+    }
+}
+
+//DETECTA EL SCROLLING PARA APLICAR LA ANIMACIÓN DE LAS BARRAS DE HABILIDADES 
+window.onscroll = function(){
+    efectoHabilidades()
+};
+
+//FUNCIÓN PARA APLICAR LA ANIMACIÓN DE LA BARRA DE HABILIDADES
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >=300){
+        document.getElementById("html").classList.add("barra-progreso1")
+        document.getElementById("js").classList.add("barra-progreso2")
+        document.getElementById("bd").classList.add("barra-progreso3")
+        document.getElementById("ps").classList.add("barra-progreso4")
+    }
+}
