1240f60b55d79c91b7e4c37f03c2acff17d27d44
Juanpmodu92 <juanpmodu92@gmail.com>
2025-01-31 10:42:03 -0500

    Cambios en servicios y habilidades

--- script.js
+++ script.js
@@ -34,8 +34,12 @@ function efectoHabilidades() {
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >=300){
         document.getElementById("html").classList.add("barra-progreso1")
-        document.getElementById("js").classList.add("barra-progreso2")
-        document.getElementById("bd").classList.add("barra-progreso3")
-        document.getElementById("ps").classList.add("barra-progreso4")
+        document.getElementById("bs").classList.add("barra-progreso2")
+        document.getElementById("js").classList.add("barra-progreso3")
+        document.getElementById("nd").classList.add("barra-progreso4")
+        document.getElementById("py").classList.add("barra-progreso5")
+        document.getElementById("rj").classList.add("barra-progreso6")
+        document.getElementById("bd").classList.add("barra-progreso7")
+        document.getElementById("ps").classList.add("barra-progreso8")
     }
 }
